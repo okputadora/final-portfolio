b3b68e9e4f98751b411e057ddac9b9f6cf483c4b
okputadora <okputadora@gmail.com>
2018-02-05 19:37:01 -0500

    added about page (and forgotten packages)

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next){
+  res.render('about', null)
+})
 module.exports = router;

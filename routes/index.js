50089219b459839db95ce1234a31b10930c51481
okputadora <okputadora@gmail.com>
2018-02-15 13:30:24 -0500

    added individual project page, error, and confirmation

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,26 @@ router.get('/about', function(req, res, next){
 router.get('/contact', function(req, res, next){
   res.render('contact', null)
 })
+
+router.post('/:action', function(req, res, next){
+  var action = req.params.action
+  if (action == 'contact'){
+    console.log(req.body);
+    res.redirect('confirmation')
+  }
+})
+
+router.get('/confirmation', function(req, res, next){
+  res.render('confirmation')
+})
+
+router.get('/project/:name', function(req, res, next){
+  var pages = ['jekart', 'crossword-constructor'];
+  var name = req.params.name;
+  // invalid page
+  if (pages.indexOf(name) == -1){
+    res.render('error', {message: "I'm sorry, but this page simply does not exist"})
+  }
+  res.render(name, null)
+})
 module.exports = router;

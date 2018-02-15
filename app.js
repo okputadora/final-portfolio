8ae42d4211ee1d1000748d0a160a44c69a547eaa
okputadora <okputadora@gmail.com>
2018-02-15 17:01:22 -0500

    started hooking up sendgrid to handle contact form

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var sendgrid = require('./routes/sendgrid');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/sendgrid', sendgrid)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

aaa9de86c2eba20f50682e0da9b6c0b5655e54d0
okputadora <okputadora@gmail.com>
2018-02-15 12:04:44 -0500

    started populating with content

---
+++ notes.md
@@ -0,0 +1,2 @@
+Following node immersive course part9 Oct-20
+Project starts at 0:51:00
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Michael McVeigh',
+    projects: {}
+  });
+
 });
 
 router.get('/about', function(req, res, next){
   res.render('about', null)
+})
+
+router.get('/contact', function(req, res, next){
+  res.render('contact', null)
 })
 module.exports = router;
